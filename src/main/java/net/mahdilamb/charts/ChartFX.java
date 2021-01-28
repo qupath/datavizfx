0eab8ebc80a4fa416e8d1a0d9c233a30f0b6c496
Mahdi Lamb <mahdilamb@gmail.com>
2021-01-28 00:06:09 +0000

    start adding plot series

--- src/main/java/net/mahdilamb/charts/ChartFX.java
+++ src/main/java/net/mahdilamb/charts/ChartFX.java
@@ -2,10 +2,37 @@ package net.mahdilamb.charts;
 
 
 import javafx.scene.text.Font;
+import net.mahdilamb.charts.plots.Plot;
+import net.mahdilamb.charts.series.PlotSeries;
 import net.mahdilamb.charts.styles.Text;
 
-public class ChartFX<L extends Plot> extends Chart<L> {
+public class ChartFX<P extends Plot<S>, S extends PlotSeries<S>> extends Chart<P, S> {
+
+
+    private static final class TextFx extends Chart.TextImpl {
+        private static javafx.scene.text.Text testText = new javafx.scene.text.Text();
+
+        @Override
+        protected void calculateSize() {
+            updateSize(testText.getLayoutBounds().getWidth(), testText.getLayoutBounds().getHeight());
+        }
+    }
+
+    private static final class ImageFX extends Chart.ImageImpl {
+
+        @Override
+        byte[] calculateBytes() {
+            //TODO
+            return new byte[0];
+        }
+    }
+
+    protected ChartFX(String title, double width, double height, P plot) {
+        super(title, width, height, plot);
+    }
+
     private static javafx.scene.text.Text testText = new javafx.scene.text.Text();
+
 
     @Override
     protected double getTextWidth(Text text) {
@@ -25,7 +52,7 @@ public class ChartFX<L extends Plot> extends Chart<L> {
 
     @Override
     protected void draw() {
-
+//TODO
     }
 
 }
